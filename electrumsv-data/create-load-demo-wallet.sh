8ba6aeb6836a41a5bc62b18d1ff44a59f49852f9
Darren Kellenschwiler <deggen@kschw.com>
2023-02-08 15:05:52 -0600

    new demo script
    Signed-off-by: Darren Kellenschwiler <deggen@kschw.com>

--- electrumsv-data/create-load-demo-wallet.sh
+++
@@ -1,4 +0,0 @@
-  ./electrum-sv daemon -v=debug --enable-node-wallet-api -rpcpassword= -walletnotify="python3 contrib/scripts/jsonrpc_wallet_event.py %s" > "/root/.electrum-sv/demo_log_$(date +%s).txt" 2>&1 &
-  ./electrum-sv -v=debug --no-password-check -wp="$(WALLET_PASSWORD)" create_jsonrpc_wallet -w demo
-  ./electrum-sv daemon -v=debug load_wallet -w demo --walletpassword="$(WALLET_PASSWORD)"
-  ./electrum-sv daemon -v=debug service_signup -w demo --walletpassword="$(WALLET_PASSWORD)"
